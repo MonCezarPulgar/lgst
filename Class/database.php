a5b35fe32eb0ba047deee03932cbe0d7d1af51d2
Mon Cezar Pulgar <pulgarmoncezar@gmail.com>
2024-07-12 15:12:15 +0800

    db

--- Class/database.php
+++ Class/database.php
@@ -2,7 +2,7 @@
 Class Database{
 	public $conn;
 	public function __construct(){
-		$this->conn=new mysqli('localhost','root','','translator');
+		$this->conn=new mysqli('localhost','u320585682_lgst','languageTranslate_123','u320585682_lgst');
 	}
 }
 ?>
