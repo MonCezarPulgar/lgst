21bebb064b06c1593d51deabb483f26919747166
Mon Cezar Pulgar <pulgarmoncezar@gmail.com>
2024-07-19 12:31:10 +0800

    addgenerateid

---
+++ generateuserid.php
@@ -0,0 +1,12 @@
+<?php
+function generateUSERID() {
+    // Get the current date and time
+    $dateTime = new DateTime();
+    
+    // Format the date and time as YYYYMMDD-HHMMSS
+    $formattedDateTime = $dateTime->format('Ymd-His');
+    
+    // Create the Plan ID
+    return 'USER-' . $formattedDateTime;
+}
+?>
