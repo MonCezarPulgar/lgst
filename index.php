b44fcd0321b2212b1f70c950f4076ce7fd08c529
Mon Cezar Pulgar <pulgarmoncezar@gmail.com>
2024-07-23 10:21:41 +0800

    responsive na

--- index.php
+++ index.php
@@ -402,7 +402,7 @@ session_start();
                     <li><a href="about.php">About</a></li>
                     <li><a href="services.php">Services</a></li>
                     <li><a href="contact.php">Contact</a></li>
-                    <li class="freetrial"><a href="freetrial.php">Free Trial</a></li>
+                    <li><a href="freetrial.php">Free Trial</a></li>
                 </ul>
             </div>
         </nav>
