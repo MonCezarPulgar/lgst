e83bb8fd7b49920efd54a89fe6e0eb4c038f204c
Mon Cezar Pulgar <pulgarmoncezar@gmail.com>
2024-07-29 11:41:36 +0800

    test

--- index.php
+++ index.php
@@ -429,7 +429,7 @@ body {
             </div>
             <div class="menu">
                 <ul>
-                    <li><a href="#home">Home</a></li>
+                    <li><a href="#home">Homes</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#features">Features</a></li>
                     <li><a href="#contact">Contact</a></li>
