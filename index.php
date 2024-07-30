97c879cf439de2a9aac706fc78868bfb90c3b5f0
Mon Cezar Pulgar <pulgarmoncezar@gmail.com>
2024-07-30 14:03:33 +0800

    fix index

--- index.php
+++ index.php
@@ -604,7 +604,7 @@ contact-text{
 			</button>
 			<div class="menu">
 				<ul>
-					<li><a href="#home">Homesss</a></li>
+					<li><a href="#home">Home</a></li>
 					<li><a href="#about">About</a></li>
 					<li><a href="#features">Features</a></li>
 					<li><a href="#contact">Contact</a></li>
